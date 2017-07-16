9d1eaf74c5c2f71a90a71e1aa68ed71872016a7a
Peter Brooks <mail@pbrooks.net>
2017-07-16 22:45:47 +0100

    Continued based improvements

---
+++ src/os/install/macos/fonts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../../utils.sh" \
+    && . "./utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_in_purple "\n   Fonts\n\n"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+brew_install "Source Code Pro (Powerline)" "font-source-code-pro-for-powerline" "caskroom/fonts" "fonts"
