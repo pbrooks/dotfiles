f56f9991f6826d129c55265c7d4b15d84a6039e3
Peter Brooks <peterb@wushustudios.com>
2020-06-09 16:02:36 +0100

    ubuntu-wsl: Basic implementation

--- src/os/setup.sh
+++ src/os/setup.sh
@@ -187,6 +187,7 @@ verify_os() {
     local os_name="$(get_os)"
     local os_version="$(get_os_version)"
 
+
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     # Check if the OS is `macOS` and
@@ -205,7 +206,7 @@ verify_os() {
     # Check if the OS is `Ubuntu` and
     # it's above the required version.
 
-    elif [ "$os_name" == "ubuntu" ]; then
+    elif [ "$os_name" == "ubuntu" ] || [ "$os_name" == "ubuntu-wsl" ]; then
 
         if is_supported_version "$os_version" "$MINIMUM_UBUNTU_VERSION"; then
             return 0
@@ -309,7 +310,8 @@ main() {
         # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
         if ! $skipQuestions; then
-            ./update_content.sh
+			echo "Skip"
+            #./update_content.sh
         fi
 
         # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
